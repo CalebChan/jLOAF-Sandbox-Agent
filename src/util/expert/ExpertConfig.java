39a5a2e686f4e1c4ae7451368a6758adb944d3c1
megardron <meganardron@cmail.carleton.ca>
2014-07-11 15:23:36 -0400

    Update ExpertConfig.java

--- src/util/expert/ExpertConfig.java
+++ src/util/expert/ExpertConfig.java
@@ -2,7 +2,7 @@ package util.expert;
 
 import util.expert.backtracking.ActonBasedExpertStrategy;
 import util.expert.backtracking.InputBasedExpertStrategy;
-import util.expert.lfo.SimpleRandomExpertStrategy;
+import util.expert.lfo.SmartRandomExpertStrategy;
 import util.expert.lfo.SmartStraightLineExpertStrategy;
 import util.expert.lfo.ZigZagExpertStrategy;
 
@@ -19,7 +19,7 @@ public class ExpertConfig {
 		STRATEGY[count++] = new ActonBasedExpertStrategy();
 		STRATEGY[count++] = new InputBasedExpertStrategy();
 		
-		STRATEGY[count++] = new SimpleRandomExpertStrategy();
+		STRATEGY[count++] = new SmartRandomExpertStrategy();
 		STRATEGY[count++] = new SmartStraightLineExpertStrategy();
                 STRATEGY[count++] = new FixedSequenceExpertStrategy();
                 STRATEGY[count++] = new EqualFixedSequenceExpertStrategy();
