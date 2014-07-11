9d1f5bc0163f9c0f7b80f89db6384aa61861c8ba
megardron <meganardron@cmail.carleton.ca>
2014-07-11 15:24:41 -0400

    Create EqualFixedSequenceExpertStrategy.java

---
+++ src/util/expert/EqualFixedSequenceExpertStrategy.java
@@ -0,0 +1,24 @@
+
+package util.expert;
+
+import util.SandboxTraceGUI;
+import util.expert.LfOExpertStrategy;
+import sandbox.Direction;
+import sandbox.creature.DirtBasedCreature;
+import agent.AbstractSandboxAgent;
+import agent.lfo.EqualFixedSequenceExpert;
+
+
+public class EqualFixedSequenceExpertStrategy extends LfOExpertStrategy{
+    
+    @Override
+    public AbstractSandboxAgent getAgent(int size, int x, int y, Direction dir) {
+        return new EqualFixedSequenceExpert(size, new DirtBasedCreature(x, y, dir));
+    }
+
+    @Override
+    public String getAgentName() {
+        return EqualFixedSequenceExpert.class.getSimpleName();
+    }
+    
+}
