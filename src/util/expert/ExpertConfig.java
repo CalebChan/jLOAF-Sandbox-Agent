6d048c24c55bd204d51b3b23188e09d012a98f4d
megardron <meganardron@cmail.carleton.ca>
2014-07-11 15:21:48 -0400

    Update ExpertConfig.java
    Added option for Fixed Sequence Agents and ZigZag Expert

--- src/util/expert/ExpertConfig.java
+++ src/util/expert/ExpertConfig.java
@@ -2,14 +2,15 @@ package util.expert;
 
 import util.expert.backtracking.ActonBasedExpertStrategy;
 import util.expert.backtracking.InputBasedExpertStrategy;
-import util.expert.lfo.SmartRandomExpertStrategy;
+import util.expert.lfo.SimpleRandomExpertStrategy;
 import util.expert.lfo.SmartStraightLineExpertStrategy;
+import util.expert.lfo.ZigZagExpertStrategy;
 
 public class ExpertConfig {
 
 	public static ExpertStrategy STRATEGY[];
 	
-	private static final int STRATEGY_NUM = 4;
+	private static final int STRATEGY_NUM = 7;
 	
 	static{
 		int count = 0;
@@ -18,8 +19,11 @@ public class ExpertConfig {
 		STRATEGY[count++] = new ActonBasedExpertStrategy();
 		STRATEGY[count++] = new InputBasedExpertStrategy();
 		
-		STRATEGY[count++] = new SmartRandomExpertStrategy();
+		STRATEGY[count++] = new SimpleRandomExpertStrategy();
 		STRATEGY[count++] = new SmartStraightLineExpertStrategy();
+                STRATEGY[count++] = new FixedSequenceExpertStrategy();
+                STRATEGY[count++] = new EqualFixedSequenceExpertStrategy();
+                STRATEGY[count++] = new ZigZagExpertStrategy();
 	}
 
 }
