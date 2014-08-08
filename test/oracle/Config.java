f033f2e358806eb8374b171d3f20f647dde7e95a
Caleb Chan <calebchan@cmail.carleton.ca>
2014-08-07 21:47:18 -0400

    - Reset K in kNN to 4 - Git not ignores log files

--- test/oracle/Config.java
+++ test/oracle/Config.java
@@ -3,7 +3,7 @@ package oracle;
 public class Config {
 
 	public static final int DEFAULT_WORLD_SIZE = 10;
-	public static final int DEFAULT_K = 20;
+	public static final int DEFAULT_K = 4;
 	
 	public static final int DEFAULT_MAX_K = 5;
 	
